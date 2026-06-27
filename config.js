e798b6e4fdd6293286b25e20f674f3f63de5239c
Jeongmin <aceseojm@users.noreply.github.com>
2026-06-27 18:44:52 +0900

    Add sellapt landing page and supporting docs

---
+++ config.js
@@ -0,0 +1,6 @@
+window.LANDING_CONFIG = {
+  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzFomPHgNwz6jI9Ax4BCFE3aT3kQFtIT07qzkkOqXaGMXS0oPTYHgX6XUrrvA5h0O2I-w/exec",
+  GA4_ID: "",
+  META_PIXEL_ID: "",
+  NAVER_CONVERSION_ID: ""
+};
