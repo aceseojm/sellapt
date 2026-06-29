a4b713f1641e045609cb5a7f8bbeedf2c87895ab
jeongmin <jeongmin@jeongminui-MacBookAir.local>
2026-06-29 11:09:17 +0900

    Configure Apps Script lead endpoint

--- lead-config.js
+++ lead-config.js
@@ -1,5 +1,5 @@
 window.BraincityLeadConfig = {
   // Apps Script 웹앱 배포 URL을 입력하세요.
   // 예: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxx/exec
-  appsScriptUrl: ""
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxwjJoqxbVTCzfTk5TJ3VMaMsycp05VSoe9Rmkp2iO36DOCqM77mk6ihzd6YP4QiGW6pA/exec"
 };
